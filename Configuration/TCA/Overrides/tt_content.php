3c82c7ba440336284dfb0b22ccc637ae429de2a3
Ronald Wopereis <woepwoep@gmail.com>
2019-02-28 11:07:53 +0100

    changed FF name

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -21,7 +21,7 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['wfqbe_pi
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['wfqbe_piquery'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     'wfqbe_piquery',
-    'FILE:EXT:wfqbe/Configuration/FlexForms/Flexform_plugin.xml'
+    'FILE:EXT:wfqbe/Configuration/FlexForms/Flexform_query.xml'
 );
 
 /**
