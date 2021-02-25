d7fe29f948f06a7022fc3ee012b281e112833dbb
Ronald Wopereis <woepwoep@gmail.com>
2021-02-25 16:56:54 +0100

    sqldate

--- Resources/Public/JavaScript/CUD_form.js
+++ Resources/Public/JavaScript/CUD_form.js
@@ -4,18 +4,32 @@ jQuery(document).ready(function() {
   
    jQuery('.datum').datepicker({
 	  'language': 'nl',
-      'format': 'dd-mm-yyyy',
+      'format': 'd-m-yyyy',
       'autoclose': true,  
       'changeMonth': true,
       'changeYear': true,
       'minDate': '01-01-2014',
+	  'todayBtn': 'linked',
+	  'todayHighlight': true,
       'showAnim': 'slideDown' });
-  
+	  
+	jQuery('.sqldate').datepicker({
+	  'language': 'nl',
+      'format': 'yyyy-m-d',
+      'autoclose': true,  
+      'changeMonth': true,
+      'changeYear': true,
+      'minDate': '01-01-2014',
+	  'todayBtn': 'linked',
+	  'todayHighlight': true,
+      'showAnim': 'slideDown' });
+			
+	
   //console.log('Huidige datum is: ' + currentDate);
   //console.log('Datum in veld is: ' + currentValue);
   if (!currentValue) {
      //Set date to current date  
-    jQuery('.datum').datepicker();
+    jQuery('.datum, .sqldate').datepicker();
   }
       
    jQuery('.tijd').timepicker({
