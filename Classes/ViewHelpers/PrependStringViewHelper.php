350595011c076f2c01c3c1649880138392a532dd
Ronald Wopereis <woepwoep@gmail.com>
2021-02-26 18:29:23 +0100

    viewhelper repair

--- Classes/ViewHelpers/PrependStringViewHelper.php
+++ Classes/ViewHelpers/PrependStringViewHelper.php
@@ -20,9 +20,10 @@ class PrependStringViewHelper extends AbstractViewHelper
     }
 
     /**
-     * Merges the two arrays .
+     * if array, prepends array_keys with string
+	 * if string, prepends parameter array with string
      *
-     * @return array
+     * @return mixed
      */
     public function render()
     {
@@ -30,8 +31,16 @@ class PrependStringViewHelper extends AbstractViewHelper
         $string = $this->arguments['string'];
         // DebugUtility::debug($array,'array in PrependStringViewHelper');
         // DebugUtility::debug($string,'string in PrependStringViewHelper');
+
+		// are parameters valid?
 		if (empty($array)) return $array;
 		if (empty($string)) return $array;
+
+		// if arg1 is a string, we're done fast
+		if (!is_array($array)) {
+			return $string . $array;
+		}
+
 		$newArray = array();
 		foreach($array AS $key => $value) {
 			$newArray['RSRQ_'.$key] = $value;
